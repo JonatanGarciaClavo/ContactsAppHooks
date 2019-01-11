042376b6ca78b7373b25602acb67634a7d02e240
Jonatan García <jonatan.garcia@mobgen.com>
2019-01-11 16:56:29 +0100

    Fixed firebase warning.

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,10 @@
-import * as firebase from "firebase";
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 const config = {
-  apiKey: "AIzaSyB1GATIyuA6FNnAUMBz0kXJ62EvlSWx99o",
-  authDomain: "redux-contacts-app.firebaseapp.com",
-  databaseURL: "https://redux-contacts-app.firebaseio.com/"
+  apiKey: 'AIzaSyB1GATIyuA6FNnAUMBz0kXJ62EvlSWx99o',
+  authDomain: 'redux-contacts-app.firebaseapp.com',
+  databaseURL: 'https://redux-contacts-app.firebaseio.com/',
 };
 
 firebase.initializeApp(config);
